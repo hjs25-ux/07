4e9454dc3759d51565ab63782e86703eb3ad75bd
hjs25-ux <hjs25@sookmyung.ac.kr>
2025-10-16 16:02:39 +0900

    05

--- main.c
+++ main.c
@@ -2,22 +2,27 @@
 #include <stdlib.h>
 
 
-void f(void);
+void sub(void);
 
-int i;
+
 int main(void)
 {
-    for (i=0;i<5;i++)
-    {
-        f();
-        }
-        system("PAUSE");
-        return 0;
-        }
-        
-    void f(void)
-    {
-         for(i=0;i<10;i++)
-         printf("#");
-         }
-         
+    int i;
+    for(i=0;i<3;i++)
+    sub();
+    
+    system("PAUSE");
+    return 0;
+}
+
+void sub(void)
+{
+     int auto_count=0;
+     static int static_count=0;
+     
+     auto_count++;
+     static_count++;
+     
+     printf("auto_count=%d\n", auto_count);
+     printf("static_count=%d\n", static_count);
+}
